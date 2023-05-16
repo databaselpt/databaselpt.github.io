a17f97e3f4df9f97a4a521b73e4fe164eee7298d
databaselpt <133755518+databaselpt@users.noreply.github.com>
2023-05-16 16:07:30 +0700

    Update reno.js

--- js/reno.js
+++ js/reno.js
@@ -27,19 +27,31 @@ function validateForm() {
     }
 
     
-        function forgotPassword() {
-        var phoneNumber = "6285750852290"; // Nomor telepon dengan kode wilayah Indonesia
-        var message = "Saya lupa username dan password login akun lpt, mohon berikan passwordnya";
-
-        // Format nomor telepon tanpa karakter "-" atau spasi
-        var formattedPhoneNumber = phoneNumber.replace(/[-\s]/g, "");
-
-        // Format pesan untuk URL
-        var encodedMessage = encodeURIComponent(message);
-
-        // URL deep linking WhatsApp
-        var whatsappURL = "https://web.whatsapp.com/send?phone=" + formattedPhoneNumber + "&text=" + encodedMessage;
-
-        // Membuka WhatsApp Web
-        window.open(whatsappURL, "_blank");
-        }
+    function forgotPassword() {
+      var phoneNumber = "6285750852290"; // Nomor telepon dengan kode wilayah Indonesia
+      var message = "Saya lupa username dan password login akun lpt, mohon berikan passwordnya";
+    
+      // Format nomor telepon tanpa karakter "-" atau spasi
+      var formattedPhoneNumber = phoneNumber.replace(/[-\s]/g, "");
+    
+      // Format pesan untuk URL
+      var encodedMessage = encodeURIComponent(message);
+    
+      // Periksa apakah pengguna menggunakan perangkat seluler
+      var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    
+      // URL WhatsApp
+      var whatsappURL = "";
+    
+      if (isMobile) {
+        // URL untuk perangkat seluler
+        whatsappURL = "https://api.whatsapp.com/send?phone=" + formattedPhoneNumber + "&text=" + encodedMessage;
+      } else {
+        // URL untuk desktop (WhatsApp Web)
+        whatsappURL = "https://web.whatsapp.com/send?phone=" + formattedPhoneNumber + "&text=" + encodedMessage;
+      }
+    
+      // Membuka WhatsApp pada perangkat pengguna
+      window.open(whatsappURL, "_blank");
+    }
+    
